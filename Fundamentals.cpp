60a41cd0cd94b7de750354eb986e02b4a4f439b9
Michael Torres Ospina <michaeltorresos92@gmail.com>
2021-01-02 16:52:33 -0500

    New Board Project

---
+++ Fundamentals.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
+
+void PrintBoard (const vector<vector<int>> board)
+{
+    for (auto v : board){
+        for (int i : v)
+        cout << i << " ";
+    }
+    cout << "\n";
+}
+
+
+int main(){ 
+    vector<vector<int>> board {{0,1,0,0,0,0},
+                               {0,1,0,0,0,0},
+                               {0,1,0,0,0,0},
+                               {0,1,0,0,0,0},
+                               {0,0,0,0,1,0}};
+
+    PrintBoard(board);
+}
